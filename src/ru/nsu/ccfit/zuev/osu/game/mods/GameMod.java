1ff35985e466ad6ded6a989c4a16442c5a86e604
Leech-in-ur-face <74518523+Leech-in-ur-face@users.noreply.github.com>
2024-07-28 18:47:14 +0800

    Update GameMod.java

--- src/ru/nsu/ccfit/zuev/osu/game/mods/GameMod.java
+++ src/ru/nsu/ccfit/zuev/osu/game/mods/GameMod.java
@@ -5,20 +5,20 @@ public enum GameMod {
      * Temporarily unranked mods (until adjusted):
      * sd, pf, sc, re, fl
      */
-    MOD_NOFAIL("nf", 0.5f),
+    MOD_NOFAIL("nf", 1f),
     MOD_AUTO("auto", 1, true),
-    MOD_EASY("es", 0.5f),
-    MOD_HARDROCK("hr", 1.06f),
-    MOD_HIDDEN("hd", 1.06f),
-    MOD_RELAX("relax", 0.001f, true),
-    MOD_AUTOPILOT("ap", 0.001f, true),
+    MOD_EASY("es", 1f),
+    MOD_HARDROCK("hr", 1.50f),
+    MOD_HIDDEN("hd", 1.75f),
+    MOD_RELAX("relax", 1.50f),
+    MOD_AUTOPILOT("ap", 1.50f),
     MOD_DOUBLETIME("dt", 1.12f),
     MOD_NIGHTCORE("nc", 1.12f),
     MOD_HALFTIME("ht", 0.3f),
     MOD_SUDDENDEATH("sd", 1),
     MOD_PERFECT("pf", 1),
     MOD_FLASHLIGHT("fl", 1.12f),
-    MOD_PRECISE("pr", 1.06f),
+    MOD_PRECISE("pr", 15f),
     MOD_REALLYEASY("re", 0.5f, true),
     MOD_SCOREV2("v2", 1, true);
 
